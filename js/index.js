e809a8e8a3aaa7ec24b581b0981e695381bc8551
¨cristina¨ <crisdvcris2017@gmail.com>
2023-11-11 01:06:17 -0300

    ¨entregafinal¨

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
+function crearTarjetasProductosInicio(productos){
+  productos.forEach(producto => {
+    const nuevaRemera = document.createElement("div");
+    nuevaRemera.classList = "tarjeta-producto"
+    nuevaRemera.innerHTML = `
+    <img src="./img/productos/${producto.id}.jpg" alt="Remera 1">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button id="toastify">Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevaRemera);
+    nuevaRemera.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+  });
+}
+crearTarjetasProductosInicio(remeras);
+
+
+
+
+const toastify = document.querySelector ("#toastify")
+
+toastify.addEventListener("click", () =>{
+  Toastify({
+  text: "Agregaste un producto al carrito",
+  className: "info",
+  duration: 2000,
+  style: {
+    background: "rgb(191, 0, 0)",
+  }
+}).showToast();
+} )
---
+++ js/remeras.js
@@ -0,0 +1,33 @@
+
+const remeras = [
+  {
+    id:1,
+    nombre: "Remera top algodón",
+    precio: 5000,
+  },
+  {
+    id:2,
+    nombre: "Remera top morley",
+    precio: 5000,
+  },
+  {
+    id:3,
+    nombre: "Musculosa morley",
+    precio: 5000,
+  },
+  {
+    id:4,
+    nombre: "Top Satén",
+    precio: 0,
+  },
+  {
+    id:5,
+    nombre: "Top morley algodón",
+    precio: 7000,
+  },
+  {
+    id:6,
+    nombre: "Musculosa morley",
+    precio: 6000,
+  }
+]

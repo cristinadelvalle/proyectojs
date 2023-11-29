2891b94bff8f3a1fd8f3620560cc6748f6949857
¨cristina¨ <crisdvcris2017@gmail.com>
2023-11-29 10:59:16 -0300

    ¨correct¨

--- js/index.js
+++ js/index.js
@@ -1,71 +1,60 @@
 const contenedorTarjetas = document.getElementById("productos-container");
 
-
-function crearTarjetasProductosInicio(productos){
-  productos.forEach(producto => {
+function crearTarjetasProductosInicio(productos) {
+  productos.forEach((producto) => {
     const nuevaRemera = document.createElement("div");
-    nuevaRemera.classList = "tarjeta-producto"
+    nuevaRemera.classList = "tarjeta-producto";
     nuevaRemera.innerHTML = `
     <img src="../img/productos/${producto.id}.jpg" alt="${producto.id} ">
     <h3>${producto.nombre}</h3>
     <p class="precio">$${producto.precio}</p>
     <button id="toastify"> Agregar al carrito</button>
-    <button id="toastify">Comprar</button>`
+    <button id="toastify">Comprar</button>`;
 
     contenedorTarjetas.appendChild(nuevaRemera);
-    nuevaRemera.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+    nuevaRemera
+      .getElementsByTagName("button")[0]
+      .addEventListener("click", () => agregarAlCarrito(producto));
   });
 }
 crearTarjetasProductosInicio(remeras);
 
- const toastify = document.querySelector ("#toastify")
+const toastify = document.querySelector("#toastify");
 
- toastify.addEventListener("click", () =>{
+toastify.addEventListener("click", () => {
   Toastify({
-  text: "Agregaste un producto al carrito",
-  className: "info",
-  duration: 2000,
-  style: {
-    background: "rgb(191, 0, 0)",
-  }
-}).showToast();
-} )
+    text: "Agregaste un producto al carrito",
+    className: "info",
+    duration: 2000,
+    style: {
+      background: "rgb(191, 0, 0)",
+    },
+  }).showToast();
+});
 
-
-
-
-const jeans = document.querySelector("#jeans")
+const jeans = document.querySelector("#jeans");
 
 fetch("./js/jeans.json")
-.then((res) => res.json())
-.then((data)=> {
-console.table(data);
-data.forEach((producto) =>{
+  .then((res) => res.json())
+  .then((data) => {
+    console.table(data);
+    data.forEach((producto) => {
+      const nuevoJean = document.createElement("div");
 
-  const nuevoJean = document.createElement("div");
-  
-    nuevoJean.classList = "tarjeta-producto"
-    nuevoJean.innerHTML = ` 
+      nuevoJean.classList = "tarjeta-producto";
+      nuevoJean.innerHTML = ` 
     <img src="../img/productos/${producto.id}.jpg" alt="${producto.id} ">
     <h3>${producto.nombre}</h3>
     <p class="precio">$${producto.precio}</p>
     <button id="toastify"> Agregar al carrito</button>
-    <button id="toastify">Comprar</button>`
-    document.createElement("section")
- 
-   
-    contenedorTarjetas.appendChild(nuevoJean);
-    nuevoJean.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+    <button id="toastify">Comprar</button>`;
+      document.createElement("section");
+
+      contenedorTarjetas.appendChild(nuevoJean);
+      nuevoJean
+        .getElementsByTagName("button")[0]
+        .addEventListener("click", () => agregarAlCarrito(producto));
+    });
   });
-})
+  
 crearTarjetasProductosInicio(jeans);
-
-
-
-
-
-
-
-
-
-
